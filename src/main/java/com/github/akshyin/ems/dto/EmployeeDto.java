565923d42808ff683aa56ad513921d27b1b35309
akshay <akshay@akhplap.akhp>
2024-08-30 17:39:14 +0530

    removed some tags

--- src/main/java/com/github/akshyin/ems/dto/EmployeeDto.java
+++ src/main/java/com/github/akshyin/ems/dto/EmployeeDto.java
@@ -9,40 +9,46 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class EmployeeDto {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	private Long id;
 	private String firstname;
 	private String lastname;
 	private String email;
+
 	public EmployeeDto(Long id, String firstname, String lastname, String email) {
 		super();
 		this.id = id;
@@ -50,10 +56,10 @@ public class EmployeeDto {
 		this.lastname = lastname;
 		this.email = email;
 	}
+
 	public EmployeeDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	
+
 }
--- src/main/java/com/github/akshyin/ems/entity/Employee.java
+++ src/main/java/com/github/akshyin/ems/entity/Employee.java
@@ -13,44 +13,51 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name="first_name")
+	@Column(name = "first_name")
 	private String firstname;
-	@Column(name="last_name")
+	@Column(name = "last_name")
 	private String lastname;
-	@Column(name="email_id",nullable = false,unique = true )
+	@Column(name = "email_id", nullable = false, unique = true)
 	private String email;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Employee(Long id, String firstname, String lastname, String email) {
 		super();
 		this.id = id;
@@ -58,9 +65,10 @@ public class Employee {
 		this.lastname = lastname;
 		this.email = email;
 	}
+
 	public Employee() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 }
